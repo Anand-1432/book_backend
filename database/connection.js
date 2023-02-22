263c23cec56db0ca89eef5d327d84cdf7d77ed9a
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2023-02-22 23:47:27 +0530

    updating

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost:27017/books').then(() => {
+mongoose.connect('mongodb+srv://Anand:Anand123@cluster0.a5kwk.mongodb.net/jecsc?retryWrites=true&w=majority').then(() => {
     console.log('connection established....!');
 }).catch((err) => {
     console.log(err);
